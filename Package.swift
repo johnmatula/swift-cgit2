b814f2d9c2111aad819e6c8ea8ebaa6b494510c4
John Matula <johnmatula@users.noreply.github.com>
2024-03-27 12:15:16 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,17 +21,17 @@ let package = Package(
 
     .binaryTarget(
       name: "OpenSSL",
-      url: "https://github.com/sharplet/swift-cgit2/releases/download/v1.1.1/OpenSSL.xcframework.zip",
+      url: "https://github.com/johnmatula/swift-cgit2/releases/download/v1.1.1/OpenSSL.xcframework.zip",
       checksum: "65afedb6e49bfc58dbb785ff771592c3f8b093e595a11c99b271ece92d400fda"
     ),
     .binaryTarget(
       name: "libgit2",
-      url: "https://github.com/sharplet/swift-cgit2/releases/download/v1.1.1/libgit2.xcframework.zip",
+      url: "https://github.com/johnmatula/swift-cgit2/releases/download/v1.1.1/libgit2.xcframework.zip",
       checksum: "433fc5d4cb892325b4469eadf1eda33ede2663340cc36857484d166cc0ad5a99"
     ),
     .binaryTarget(
       name: "libssh2",
-      url: "https://github.com/sharplet/swift-cgit2/releases/download/v1.1.1/libssh2.xcframework.zip",
+      url: "https://github.com/johnmatula/swift-cgit2/releases/download/v1.1.1/libssh2.xcframework.zip",
       checksum: "ceceaeaf84cc2e89a61016c6951ba5bd27a75e3d0cd8ae21e328ad158ace340d"
     ),
   ]
